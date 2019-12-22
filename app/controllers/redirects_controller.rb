56082299d478340114d3c172ecd268dc52823052
Alex Leasenco <rubydeve@gmail.com>
2019-12-22 23:40:53 +0200

    Redirect user from shorten link to long

---
+++ app/controllers/redirects_controller.rb
@@ -0,0 +1,10 @@
+class RedirectsController < ApplicationController
+  def go
+    link = current_user.links.find_by(short_url: params[:slug])
+    if link
+      redirect_to link.long_url
+    else
+      render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   get 'login' => 'user_sessions#new', as: :login
   post 'logout' => 'user_sessions#destroy', as: :logout
+
+  get ':slug' => 'redirects#go', as: :go
 end
