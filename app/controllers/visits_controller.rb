d2c330c5375a74aee3451682f1b409709c3aa659
Alex Leasenco <rubydeve@gmail.com>
2019-12-23 12:17:58 +0200

    Show browsers devices in chart

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,8 +1,27 @@
 class VisitsController < ApplicationController
+  before_action :set_visits
+
   # GET /visits
   # GET /visits.json
   def index
-    visits = current_user.links.find(params[:link_id]).visits
-    @visits = visits.group("date(created_at)").count
+    @visits = @visits.group("date(created_at)").count
+  end
+
+  # GET /browsers
+  # GET /browsers.json
+  def browsers
+    @visits = @visits.group(:browser_name).count
+  end
+
+  # GET /devices
+  # GET /devices.json
+  def devices
+    @visits = @visits.group(:device_name).count
+  end
+
+  private
+
+  def set_visits
+    @visits = current_user.links.find(params[:link_id]).visits
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'links#index'
   resources :user_sessions, only: %i[new create destroy]
   resources :users, only: %i[show new edit create update] do
     resources :links, only: %i[index show new create] do
       resources :visits, only: :index
+      get 'browsers' => 'visits#browsers'
+      get 'devices' => 'visits#devices'
     end
   end
 
